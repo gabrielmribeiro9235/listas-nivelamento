536b057b6140d532a6fad36c5996898e300b4f32
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-07-30 15:50:32 -0300

    feat: add solution for the Ex03 of the list01

--- src/main/java/br/edu/ifsp/list01/Ex03.java
+++ src/main/java/br/edu/ifsp/list01/Ex03.java
@@ -1,5 +1,7 @@
 package br.edu.ifsp.list01;
 
+import java.util.Scanner;
+
 /*
     Escrever um programa que, dado um ano válido qualquer, verifica se ele é bissexto ou não:
 
@@ -18,14 +20,20 @@ public class Ex03 {
 
     public static void main(String[] args) {
         //Leia o input
+        Scanner scanner = new Scanner(System.in);
+        int ano = scanner.nextInt();
         //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
+        Ex03 ex = new Ex03();
         //Escreva o resultado da chamada do método compute() aqui
+        System.out.println(ex.compute(ano));
     }
 
 
     String compute(int input) {
-        String output = null;
-        //put your logic here
-        return output;
+        if (input <= 0) return "Erro";
+
+        if (input % 4 == 0 && input % 100 != 0 || input % 400 == 0) return "Ano bissexto";
+
+        return "Ano nao bissexto";
     }
 }
