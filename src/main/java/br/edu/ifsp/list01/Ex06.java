4fa9e5c47234fedb0098dfd37c7fb9d7ceee11a0
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-07-30 21:31:39 -0300

    feat: add solution for the Ex06 of the list01

--- src/main/java/br/edu/ifsp/list01/Ex06.java
+++ src/main/java/br/edu/ifsp/list01/Ex06.java
@@ -1,5 +1,8 @@
 package br.edu.ifsp.list01;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /*
     Sabe-se que uma lata de tinta tem um custo C e é capaz de pintar uma área de M metros quadrados.
     Faça um programa que leia a largura L, a altura A de uma parede, o valor C de uma lata de tinta e
@@ -23,13 +26,20 @@ public class Ex06 {
 
     public static void main(String[] args) {
         //Leia o input
+        Scanner scanner = new Scanner(System.in);
+        double L = scanner.nextDouble();
+        double a = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        double m = scanner.nextDouble();
         //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
+        Ex06 ex = new Ex06();
         //Escreva o resultado da chamada do método compute() aqui
+        System.out.println(ex.compute(L, a, c, m));
     }
 
     String compute(double L, double a, double c, double m) {
-        String output = null;
-        //put your logic here
-        return output;
+        if (L < 0 || a < 0 || c < 0 || m <= 0) return "0 0.00";
+
+        return String.format(Locale.US,"%d %.2f", (int) Math.ceil(L * a / m), c * Math.ceil(L * a / m));
     }
 }
